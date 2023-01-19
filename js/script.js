03d907780ecde2cac0572b70162166539826b14f
Shadab Syed <shadabsyed488@gmail.com>
2023-01-19 11:40:32 +0500

    push file

---
+++ js/script.js
@@ -0,0 +1,80 @@
+document.querySelector(".button-red").addEventListener("click", function () {
+  document.querySelector(".button-blue").style.backgroundColor = "";
+  document.querySelector(".button-gold").style.backgroundColor = "";
+  document.querySelector(".button-maroon").style.backgroundColor = "";
+  document.querySelector(".button-green").style.backgroundColor = "";
+  document.querySelector(".button-aqua").style.backgroundColor = "";
+  document.querySelector(".button-purple").style.backgroundColor = "";
+  document.querySelector(".button-cyan").style.backgroundColor = "";
+  document.querySelector(".button-red").style.backgroundColor = "Red";
+});
+document.querySelector(".button-blue").addEventListener("click", function () {
+  document.querySelector(".button-red").style.backgroundColor = "";
+  document.querySelector(".button-gold").style.backgroundColor = "";
+  document.querySelector(".button-maroon").style.backgroundColor = "";
+  document.querySelector(".button-green").style.backgroundColor = "";
+  document.querySelector(".button-aqua").style.backgroundColor = "";
+  document.querySelector(".button-purple").style.backgroundColor = "";
+  document.querySelector(".button-cyan").style.backgroundColor = "";
+  document.querySelector(".button-blue").style.backgroundColor = "blue";
+});
+document.querySelector(".button-gold").addEventListener("click", function () {
+  document.querySelector(".button-blue").style.backgroundColor = "";
+  document.querySelector(".button-red").style.backgroundColor = "";
+  document.querySelector(".button-maroon").style.backgroundColor = "";
+  document.querySelector(".button-green").style.backgroundColor = "";
+  document.querySelector(".button-aqua").style.backgroundColor = "";
+  document.querySelector(".button-purple").style.backgroundColor = "";
+  document.querySelector(".button-cyan").style.backgroundColor = "";
+  document.querySelector(".button-gold").style.backgroundColor = "gold";
+});
+document.querySelector(".button-aqua").addEventListener("click", function () {
+  document.querySelector(".button-gold").style.backgroundColor = "";
+  document.querySelector(".button-blue").style.backgroundColor = "";
+  document.querySelector(".button-red").style.backgroundColor = "";
+  document.querySelector(".button-maroon").style.backgroundColor = "";
+  document.querySelector(".button-green").style.backgroundColor = "";
+  document.querySelector(".button-purple").style.backgroundColor = "";
+  document.querySelector(".button-cyan").style.backgroundColor = "";
+  document.querySelector(".button-aqua").style.backgroundColor = "aqua";
+});
+document.querySelector(".button-green").addEventListener("click", function () {
+  document.querySelector(".button-blue").style.backgroundColor = "";
+  document.querySelector(".button-gold").style.backgroundColor = "";
+  document.querySelector(".button-red").style.backgroundColor = "";
+  document.querySelector(".button-maroon").style.backgroundColor = "";
+  document.querySelector(".button-aqua").style.backgroundColor = "";
+  document.querySelector(".button-purple").style.backgroundColor = "";
+  document.querySelector(".button-cyan").style.backgroundColor = "";
+  document.querySelector(".button-green").style.backgroundColor = "green";
+});
+document.querySelector(".button-maroon").addEventListener("click", function () {
+  document.querySelector(".button-green").style.backgroundColor = "";
+  document.querySelector(".button-blue").style.backgroundColor = "";
+  document.querySelector(".button-gold").style.backgroundColor = "";
+  document.querySelector(".button-red").style.backgroundColor = "";
+  document.querySelector(".button-aqua").style.backgroundColor = "";
+  document.querySelector(".button-purple").style.backgroundColor = "";
+  document.querySelector(".button-cyan").style.backgroundColor = "";
+  document.querySelector(".button-maroon").style.backgroundColor = "maroon";
+});
+document.querySelector(".button-purple").addEventListener("click", function () {
+  document.querySelector(".button-maroon").style.backgroundColor = "";
+  document.querySelector(".button-blue").style.backgroundColor = "";
+  document.querySelector(".button-green").style.backgroundColor = "";
+  document.querySelector(".button-gold").style.backgroundColor = "";
+  document.querySelector(".button-red").style.backgroundColor = "";
+  document.querySelector(".button-aqua").style.backgroundColor = "";
+  document.querySelector(".button-cyan").style.backgroundColor = "";
+  document.querySelector(".button-purple").style.backgroundColor = "purple";
+});
+document.querySelector(".button-cyan").addEventListener("click", function () {
+  document.querySelector(".button-purple").style.backgroundColor = "";
+  document.querySelector(".button-gold").style.backgroundColor = "";
+  document.querySelector(".button-blue").style.backgroundColor = "";
+  document.querySelector(".button-green").style.backgroundColor = "";
+  document.querySelector(".button-red").style.backgroundColor = "";
+  document.querySelector(".button-maroon").style.backgroundColor = "";
+  document.querySelector(".button-aqua").style.backgroundColor = "";
+  document.querySelector(".button-cyan").style.backgroundColor = "cyan";
+});
